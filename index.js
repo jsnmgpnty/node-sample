31f523822327353a40a915ee7c2750e37abbca2a
jsnmgpnty <jsnmgpnty@gmail.com>
2021-04-19 15:47:09 +0800

    dynamic app port

--- index.js
+++ index.js
@@ -5,19 +5,20 @@ const axios = require('axios');
 const mongodb = require('mongodb');
 
 // Constants
-const PORT = 8080;
 const HOST = '0.0.0.0';
 let db;
 const APP_NAME = process.env.APP_NAME ?? 'APP1';
-let CONN_STRING = process.env.MONGODB_CONN_STRING ?? 'mongodb://localhost:27017/xccelerate_jobs';
+const CONN_STRING = process.env.MONGODB_CONN_STRING ?? 'mongodb://localhost:27017/xccelerate_jobs';
+const APP_PORT = process.env.APP_PORT ?? 8080;
 
 // App
 const app = express();
 app.get('/', (req, res) => {
   res.send(`Hello World from ${APP_NAME}`);
 });
-app.get('/app2', (req, res) => {
-  axios.get('https://node-app2')
+app.get('/other-app', (req, res) => {
+  const url = APP_NAME === 'APP1' ? 'node-app2' : 'node-app';
+  axios.get(`https://${url}`)
     .then(resp => {
       res.send(resp.data);
     })
@@ -36,13 +37,13 @@ mongodb.connect(
   { useNewUrlParser: true, useUnifiedTopology: true },
   function (err, client) {
     if (err) {
-      app.listen(PORT, HOST, () => {
-        console.log(`${APP_NAME} Running on http://${HOST}:${PORT}`);
+      app.listen(APP_PORT, HOST, () => {
+        console.log(`${APP_NAME} Running on http://${HOST}:${APP_PORT}`);
       });
     } else {
       db = client.db();
-      app.listen(PORT, HOST, () => {
-        console.log(`${APP_NAME} Running on http://${HOST}:${PORT}`);
+      app.listen(APP_PORT, HOST, () => {
+        console.log(`${APP_NAME} Running on http://${HOST}:${APP_PORT}`);
       });
     }
   }
